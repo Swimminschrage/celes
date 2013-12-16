d76aa7e754f80a5421adaf9d805ef340474d6216
Andy Schrage <ajschrag@mtu.edu>
2013-12-15 22:35:15 -0800

    Adding options for specifying the min length for text to be considered a snippet and how long to trim the snippet

--- lib/celes.rb
+++ lib/celes.rb
@@ -3,14 +3,15 @@ require 'net/http'
 require 'nokogiri'
 
 class Celes
-
   attr_reader :url
 
   def initialize(options = {})
-    raise 'No URL provided' unless options['url']
+    raise 'No URL provided' unless options[:url]
 
-    @url = options['url']
+    @url = options[:url]
     @uri = URI(@url)
+
+    parse_options(options)
   end
 
   def snippets
@@ -26,6 +27,16 @@ class Celes
   end
 
   private
+
+  def parse_options(options)
+    # Init defaults as necessary
+    @snip_length = options[:snip_length] || 140
+    @min_snippet_length = options[:min_snippet_length] || 40
+
+    # Normalize bad options
+    @min_snippet_length = 0 if @min_snippet_length < 0
+    @snip_length = 0 if @snip_length < 0
+  end
 
   def make_request
     resp = get_html_content
@@ -50,8 +61,11 @@ class Celes
   def parse_html (response)
     doc = Nokogiri::HTML.parse(response)
 
-    @images = doc.xpath('//img/@src').map{ |x| x.value }
-    @snippets = doc.xpath('//*[self::p or self::span]/text()').map{ |x| x.to_s }
-  end
+    @images = doc.xpath('/html/body//img/@src').map{ |x| x.value }
+    @snippets = doc.xpath('/html/body//*[self::p or self::span]/text()').map{ |x| x.to_s}.keep_if do |str|
+      str.strip.length >= @min_snippet_length
+    end
 
+    @snippets = @snippets.map { |x| x[0, @snip_length] }
+  end
 end
--- spec/celes_spec.rb
+++ spec/celes_spec.rb
@@ -4,7 +4,7 @@ require 'uri'
 
 describe Celes do
   let(:url) {'http://www.theredheadproject.com'}
-  let(:celes) { Celes.new('url' => url) }
+  let(:celes) { Celes.new(url: url) }
   subject{celes}
 
   it {should respond_to(:images)}
@@ -20,7 +20,7 @@ describe Celes do
 
     context 'that causes a redirect' do
       let(:url) {'http://google.com'} # Should redirect the user to http://www.google.com
-      let(:celes) { Celes.new('url' => url) }
+      let(:celes) { Celes.new(url: url) }
       subject{celes}
 
       its(:get_html_content) {should_not be nil}
@@ -28,7 +28,7 @@ describe Celes do
 
     describe 'that contains images and text snippets' do
       before do
-        subject.stub(:get_html_content) { '<html><body><p>This is a test</p><img src="myimg.png" width="100" height="100"/></body></html>' }
+        subject.stub(:get_html_content) { '<html><body><p>This is a test for some text that should be long enough</p><img src="myimg.png" width="100" height="100"/></body></html>' }
       end
 
       it 'should have only 1 image' do
@@ -40,7 +40,102 @@ describe Celes do
       it 'should have only 1 snippet' do
         expect(subject.snippets).to be_an Array
         expect(subject.snippets.size).to be 1
-        expect(subject.snippets[0]).to be == 'This is a test'
+        expect(subject.snippets[0]).to be == 'This is a test for some text that should be long enough'
+      end
+    end
+
+    # Marking these tests as pending until I can figure out how to get Nokogiri to parse html that has img and p tags
+    # in the head.
+    describe 'only parses images and text snippets within the body' do
+      let(:url) {'http://www.theredheadproject.com'}
+      let(:nobody) { Celes.new(url: url) }
+      subject {nobody}
+      before do
+        nobody.stub(:get_html_content).and_return '<html><head><p>This is a test for some text that should be long enough</p><img src="myimg.png" width="100" height="100"/></head><body><p>Body Text</p></body></html>'
+      end
+
+      xit 'should have no images' do
+        expect(subject.images).to be_an Array
+        subject.images.each { |img| puts img }
+        expect(subject.images.size).to be 0
+
+      end
+
+      xit 'should have no snippets' do
+        expect(subject.snippets).to be_an Array
+        expect(subject.snippets.size).to be 0
+      end
+    end
+
+    describe 'can take options' do
+      context ':min_snippet_length' do
+        it 'defaults to 40 characters' do
+          c = Celes.new(url: url)
+          c.stub(:get_html_content).and_return '<p>Not 40 characters</p><p>Definitely 40 characters long test here!</p>'
+
+          expect(c.snippets.size).to be 1
+          expect(c.snippets[0]).to eq 'Definitely 40 characters long test here!'
+        end
+
+        it 'less than 0 defaults to 0' do
+          c = Celes.new(url: url, min_snippet_length: -10)
+          c.stub(:get_html_content).and_return '<p>Not 40 characters</p><p>Definitely 40 characters long test here!</p>'
+
+          expect(c.snippets.size).to be 2
+          expect(c.snippets[0]).to eq 'Not 40 characters'
+          expect(c.snippets[1]).to eq 'Definitely 40 characters long test here!'
+        end
+
+        it 'can be set to any int 0 or greater' do
+          c = Celes.new(url: url, min_snippet_length: 17)
+          c.stub(:get_html_content).and_return '<p>Not 40 characters</p><p>Definitely 40 characters long test here!</p>'
+
+          expect(c.snippets.size).to be 2
+          expect(c.snippets[0]).to eq 'Not 40 characters'
+          expect(c.snippets[1]).to eq 'Definitely 40 characters long test here!'
+        end
+
+        it 'can be set to 0' do
+          c = Celes.new(url: url, min_snippet_length: 0)
+          c.stub(:get_html_content).and_return '<p>A</p><p>Not 40 characters</p><p>Definitely 40 characters long test here!</p>'
+
+          expect(c.snippets.size).to be 3
+          expect(c.snippets[0]).to eq 'A'
+        end
+      end
+
+      context ':snip_length' do
+        let(:text59) { 'C' * 59 }
+        let(:text140) { 'A' * 140 }
+        let(:text150) { 'B' * 150 }
+
+        before {Celes.any_instance.stub(:get_html_content).and_return "<p>#{text59}</p><p>#{text140}</p><p>#{text150}</p>"}
+
+        it 'defaults to 140 characters' do
+          c = Celes.new(url: url)
+
+          expect(c.snippets.size).to be 3
+          expect(c.snippets[0]).to eq text59
+          expect(c.snippets[1]).to eq text140
+          expect(c.snippets[2]).to eq text150[0, 140]
+        end
+
+        it 'defaults to 0 when a negative value is provided' do
+          c = Celes.new(url: url, snip_length: -100)
+
+          expect(c.snippets.size).to be 3
+          expect(c.snippets[0]).to be_empty
+          expect(c.snippets[1]).to be_empty
+        end
+
+        it 'can be set to be any value greater than 0' do
+          c = Celes.new(url: url, snip_length: 60)
+
+          expect(c.snippets.size).to be 3
+          expect(c.snippets[0]).to eq text59
+          expect(c.snippets[1]).to eq text140[0, 60]
+          expect(c.snippets[2]).to eq text150[0, 60]
+        end
       end
     end
   end
